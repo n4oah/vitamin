26df975262fbccefd924bdce3529c0d29d30cf92
DESKTOP-43PFTP9\n4oah <n4oahdev@gmail.com>
2018-01-07 15:04:36 +0900

    기업 회원가입

--- vitamin/src/main/java/kr/co/vitamin/repository/vo/account/AccountSignup.java
+++ vitamin/src/main/java/kr/co/vitamin/repository/vo/account/AccountSignup.java
@@ -15,8 +15,10 @@ public class AccountSignup extends Account {
 		super.setAccountNo(accountNo);
 		if(member != null) {
 			member.setAccountNo(accountNo);
+			System.out.println("맘 배");
 		} else if(company != null) {
 			company.setAccountNo(accountNo);			
+			System.out.println("룬루");
 		}
 	}
 
